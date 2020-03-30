eb43a1fe372132b3fc82001b5372e9158b5f2476
Z1K_3C <tripleec13@hotmail.com>
2020-03-30 17:17:56 -0600

    Second modification

--- index.js
+++ index.js
@@ -9,5 +9,13 @@ app.use(express.static(path.join(__dirname, './public')));
 app.listen(app.get('port'), function(){                  
   console.log(`Server on port ${app.get('port')}`);    
 });
+/*
+git notes
 
+git init
+git add README.md //optional
+git commit -m "anything comment"
+git remote add origin https://github.com/Z1K3C/"name of your repository"
+git push -u origin master
 
+*/
